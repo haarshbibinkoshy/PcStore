396358609928bc9a2a8918aea2058baab2231916
Ladeed <muhammed.ladeed123@gmail.com>
2023-02-09 11:27:10 +0530

    Added basic design

--- pcstore-react-app/src/App.js
+++ pcstore-react-app/src/App.js
@@ -1,39 +1,42 @@
-import {styled} from "@mui/material"
-import { Box } from "@mui/system";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import Slider from "react-slick";
+import { styled } from "@mui/material"
+import { Box } from "@mui/system"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import Slider from "react-slick"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import ItemPage from "./pages/ItemPage";
+import ItemPage from "./pages/ItemPage"
+import ComparePage from "./pages/ComparePage"
 
 function App() {
 
   return (
     <Box sx={{
-      backgroundColor:"#ececec",
+      backgroundColor: "#ececec",
       // backgroundColor:"black",
-      height:"100%"}}>
-
-    
-
-    <Navbar/>
-    <Box sx={{display:'flex', flexDirection: 'column', justifyContent:'space-between', alignItems: 'center',
-    // minHeight:'100vh'
+      height: "100%"
     }}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/itemPage/:id" element={<ItemPage/>}/>
-        {/* <Route path="home" component={HomePage}/> */}
-      </Routes>
-    </Router>
-      {/* <HomePage/> */}
-      <Footer/>
-    </Box>
+
+
+
+      <Navbar />
+      <Box sx={{
+        display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center',
+        // minHeight:'100vh'
+      }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/itemPage/:id" element={<ItemPage />} />
+            <Route path="/comparePage" element={<ComparePage />} />
+          </Routes>
+        </Router>
+        {/* <HomePage/> */}
+        <Footer />
+      </Box>
 
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ pcstore-react-app/src/pages/data.js
@@ -0,0 +1,497 @@
+
+let data = [
+    {
+        "_id": "63de5f5a2a98c27336649841",
+        "name": "Apple MacBook Air M1 MGN93HN/A Ultrabook (Apple M1/8 GB/256 GB SSD/macOS Big Sur)",
+        "price": "86990",
+        "brand": "Apple\n",
+        "category": [
+            "Laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517513212apple-m1-mgn93hn-a-141586-v1-large-1.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517513212apple-m1-mgn93hn-a-141586-v1-large-2.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517513212apple-m1-mgn93hn-a-141586-v1-large-3.webp"
+        ],
+        "generalInformation": {
+            "brand": "Apple\n",
+            "model": "M1 MGN93HN/A\n",
+            "thickness": "10.9 Millimeter thickness (Slim)\n",
+            "dimensions(WxDxH)": "304.1 x 212.4 x 10.9  mm\n",
+            "weight": "1.29 Kg weight (Light-weight)\n",
+            "colour(s)": "Silver\n",
+            "operatingSystem": "macOS Big Sur"
+        },
+        "displayDetails": {
+            "displaySize": "13.3 Inches (33.78 cm)\n",
+            "displayResolution": "2560 x 1600 Pixels\n",
+            "pixelDensity": "227 ppi\n",
+            "displayType": "LED\n",
+            "displayFeatures": "Quad LED Backlit IPS Display (227 PPI\n400 nits Brightness\nWide Colour (P3)\nTrue Tone Technology)",
+            "touchScreen": "No"
+        },
+        "performance": {
+            "processor": "Apple M1",
+            "clockSpeed": "",
+            "graphicProcessor": ""
+        },
+        "memory": {
+            "capacity": "8 GB\n",
+            "ramType": "DDR4\n",
+            "memorySlots": "1",
+            "memoryLayout": "1x8 Gigabyte"
+        },
+        "storage": {
+            "ssdCapacity": "256 GB"
+        },
+        "battery": {
+            "batteryCell": "",
+            "batteryType": "Li-Po\n",
+            "powerSupply": "30 W AC Adapter W\n"
+        },
+        "networking": {
+            "wirelessLAN": "802.11 b/g/n/ax\n",
+            "wifiVersion": "6",
+            "bluetooth": "Yes",
+            "bluetoothVersion": "5.0"
+        },
+        "ports": {
+            "usb2.0slots": "2",
+            "headphoneJack": "Yes",
+            "microphoneJack": "Yes"
+        },
+        "multimedia": {
+            "webcam": "Yes",
+            "videoRecording": "720p HD\n",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "Built-in Speakers\n",
+            "inbuiltMicrophone": "Yes\n",
+            "microphoneType": "Three-mic Array with Directional Beamforming",
+            "secondarycam(Rear-facing)": "No"
+        },
+        "peripherals": {
+            "pointingDevice": "Touchpad with Multi-Touch Gestures Enabled\n",
+            "keyboard": "Magic Keyboard\n",
+            "backlitKeyboard": "Yes",
+            "fingerprintScanner": "No"
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": "Laptop, Battery, AC Adapter, User Guide\n",
+            "warranty": "1 Year\n"
+        },
+        "createdAt": "2023-02-02T13:53:22.908Z"
+    },
+    {
+        "_id": "63de5b752a98c2733664983f",
+        "name": "Acer Swift 5 Laptop (Core i5 11th Gen/8 GB/512 GB SSD/Windows 11) SF514-55TA (NX.A6SSI.005)",
+        "price": "79999",
+        "brand": "Acer",
+        "category": [
+            "Laptop",
+            "Gaming laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675516375977acer-sf514-55ta-nx-a6ssi-005-151435-v1-large-1.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675516375978acer-sf514-55ta-nx-a6ssi-005-151435-v1-large-2.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675516375978acer-sf514-55ta-nx-a6ssi-005-151435-v1-large-3.webp"
+        ],
+        "generalInformation": {
+            "brand": "Acer",
+            "model": "SF514-55TA (NX.A6SSI.005)\n",
+            "thickness": "14.9 Millimeter thickness (Slim)\n",
+            "dimensions(WxDxH)": "318.9 x 206.98 x 14.9  mm\n",
+            "weight": "1.05 Kg weight (Light-weight)\n",
+            "colour(s)": "Mist Green\n",
+            "operatingSystem": "Windows 11 Home Basic\n"
+        },
+        "displayDetails": {
+            "displaySize": "14 Inches (35.56 cm)\n",
+            "displayResolution": "1920 x 1080 Pixels\n",
+            "pixelDensity": "157 ppi\n",
+            "displayType": "",
+            "displayFeatures": "Full HD\nIPS (In-Plane Switching) technology\n(High-brightness (340 nits) Acer ComfyView LED-backlit TFT LCD\nsupporting multi-touch\n16:9 aspect ratio\ncolor gamut sRGB 100% Display)",
+            "touchScreen": "Yes"
+        },
+        "performance": {
+            "processor": "Intel Core i5-1135G7 (11th Gen)\n",
+            "clockSpeed": "4.2 Ghz\n",
+            "graphicProcessor": "Intel Integrated Iris Xe"
+        },
+        "memory": {
+            "capacity": "8 GB\n",
+            "ramType": "LPDDR4\n",
+            "memorySlots": "1",
+            "memoryLayout": "1*8 Gigabyte"
+        },
+        "storage": {
+            "ssdCapacity": "512 GB"
+        },
+        "battery": {
+            "batteryCell": "4 Cell\n",
+            "batteryType": "Li-Ion\n",
+            "powerSupply": "65 W\n"
+        },
+        "networking": {
+            "wirelessLAN": "802.11 a/b/g/n/ac\n",
+            "wifiVersion": "5",
+            "bluetooth": "Yes\n",
+            "bluetoothVersion": "5.1"
+        },
+        "ports": {
+            "usb2.0slots": "1",
+            "headphoneJack": "Yes",
+            "microphoneJack": "Yes"
+        },
+        "multimedia": {
+            "webcam": "Yes",
+            "videoRecording": "720\n",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "DTS Audio, featuring optimized bass response and micro-speaker distortion prevention, Acer TrueHarmony technology",
+            "inbuiltMicrophone": "Yes",
+            "microphoneType": "Dual built-in microphones",
+            "secondarycam(Rear-facing)": "Two built-in front facing stereo speakers\n"
+        },
+        "peripherals": {
+            "pointingDevice": "Touchpad with Multi-touch Gesture Support\n",
+            "keyboard": "83-/84-/87-key full-size FineTip keyboard with international language support\n",
+            "backlitKeyboard": "Yes\n",
+            "fingerprintScanner": "Yes\n"
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": "Laptop, Power Adaptor, User Guide, Warranty Documents\n",
+            "warranty": "1 Year\n"
+        },
+        "createdAt": "2023-02-04T13:53:23.908Z"
+    },
+    {
+        "_id": "63de0f31d0bc95de621f9cbe",
+        "name": "Acer Swift 3 Laptop (AMD Hexa Core Ryzen 5/8 GB/512 GB SSD/Windows 11) SF314-43 (NX.AB1SI.007)",
+        "price": "59999",
+        "brand": "Acer",
+        "category": [
+            "Laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675497000350acer-sf314-43-nx-ab1si-007-147407-v1-large-1.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675497000351acer-sf314-43-nx-ab1si-007-147407-v1-large-2.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675497000352acer-sf314-43-nx-ab1si-007-147407-v1-large-3.webp"
+        ],
+        "generalInformation": {
+            "brand": "Acer",
+            "model": "SF314-43 (NX.AB1SI.007)",
+            "thickness": "15.9 Millimeter thickness (Slim)",
+            "dimensions(WxDxH)": "322 x 212 x 15.9  mm",
+            "weight": "1.2 Kg weight (Light-weight)",
+            "colour(s)": "Silver",
+            "operatingSystem": "Windows 11 Home Basic"
+        },
+        "displayDetails": {
+            "displaySize": "14 Inches (35.56 cm)",
+            "displayResolution": "1920 x 1080 Pixels",
+            "pixelDensity": "157 ppi",
+            "displayType": "LED",
+            "displayFeatures": "Full HD Display with IPS (In-Plane Switching) technology\nHigh-brightness (300nits) Acer ComfyView LED-backlit TFT LCD",
+            "touchScreen": "No"
+        },
+        "performance": {
+            "processor": "AMD Hexa Core Ryzen 5 5500U\n",
+            "clockSpeed": "2.1 Ghz\n",
+            "graphicProcessor": "AMD Radeon"
+        },
+        "memory": {
+            "capacity": "8 GB\n",
+            "ramType": "LPDDR4X",
+            "memorySlots": "1",
+            "memoryLayout": "1x8 Gigabyte"
+        },
+        "storage": {
+            "ssdCapacity": "512 GB"
+        },
+        "battery": {
+            "batteryCell": "3 Cell\n",
+            "batteryType": "Li-Ion\n",
+            "powerSupply": "48 W AC Adapter W"
+        },
+        "networking": {
+            "wirelessLAN": "802.11 b/g/n/ax\n",
+            "wifiVersion": "6",
+            "bluetooth": "Yes",
+            "bluetoothVersion": "5.2"
+        },
+        "ports": {
+            "usb2.0slots": "1",
+            "headphoneJack": "Yes",
+            "microphoneJack": "Yes"
+        },
+        "multimedia": {
+            "webcam": "Yes",
+            "videoRecording": "720p HD\n",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "Built-in Speakers\n",
+            "inbuiltMicrophone": "Yes",
+            "microphoneType": "Built-in Microphone",
+            "secondarycam(Rear-facing)": "No"
+        },
+        "peripherals": {
+            "pointingDevice": "Multi-gesture touchpad, supporting two-finger scroll; pinch; gestures to open Cortana, Action Center, multitasking; application commands",
+            "keyboard": "83-/84-/87-key full-size FineTip keyboard with international language support\n",
+            "backlitKeyboard": "Yes",
+            "fingerprintScanner": "No"
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": "Laptop, Battery, AC Adapter, User Guide\n",
+            "warranty": "1 Year\n"
+        },
+        "createdAt": "2023-02-02T13:53:26.908Z"
+    },
+    {
+        "_id": "63de63527194b41884d4693a",
+        "name": "HP Pavilion 15-ec2004AX (3E3R5PA) Laptop (AMD Hexa Core Ryzen 5/8 GB/512 GB SSD/Windows 10/4 GB)",
+        "price": "56250",
+        "brand": "HP",
+        "category": [
+            "Laptop",
+            "Gaming laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675518796487hp-pavilion-15-ec2004ax-3e3r5pa-laptop-amd-hexa-core-ryzen-5-8-gb-512-gb-ssd-windows-10-4-gb-146510-v3-large-1.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675518796487hp-pavilion-15-ec2004ax-3e3r5pa-laptop-amd-hexa-core-ryzen-5-8-gb-512-gb-ssd-windows-10-4-gb-146510-v3-large-2.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675518796488hp-pavilion-15-ec2004ax-3e3r5pa-laptop-amd-hexa-core-ryzen-5-8-gb-512-gb-ssd-windows-10-4-gb-146510-v3-large-3.webp"
+        ],
+        "generalInformation": {
+            "brand": "HP",
+            "model": "15-ec2004AX (3E3R5PA)\n",
+            "thickness": "23.5 Millimeter thickness\n",
+            "dimensions(WxDxH)": "360 x 257 x 23.5  mm\n",
+            "weight": "1.98 Kg weight\n",
+            "colour(s)": "Shadow Black\n",
+            "operatingSystem": "Windows 10 Home Basic\n"
+        },
+        "displayDetails": {
+            "displaySize": "15.6 Inches (39.62 cm)\n",
+            "displayResolution": "1920 x 1080 Pixels\n",
+            "pixelDensity": "141 ppi\n",
+            "displayType": "LED\n",
+            "displayFeatures": "Full HD WLED-Backlit IPS Anti-glare Micro-edge Display (Brightness: 250 nits\n141 PPI\nColor Gamut: 45% NTSC)",
+            "touchScreen": "No\n"
+        },
+        "performance": {
+            "processor": "AMD Hexa Core Ryzen 5 - 5600H\n",
+            "clockSpeed": "3.3 Ghz\n",
+            "graphicProcessor": "NVIDIA GeForce GTX 1650\n"
+        },
+        "memory": {
+            "capacity": "8 GB\n",
+            "ramType": "DDR4\n",
+            "memorySlots": "1",
+            "memoryLayout": "1x8 Gigabyte"
+        },
+        "storage": {
+            "ssdCapacity": "512 GB"
+        },
+        "battery": {
+            "batteryCell": "3 Cell\n",
+            "batteryType": "Li-Ion\n",
+            "powerSupply": "52.5 W AC Adapter W\n"
+        },
+        "networking": {
+            "wirelessLAN": "802.11 a/b/g/n/ac\n",
+            "wifiVersion": "5",
+            "bluetooth": "Yes",
+            "bluetoothVersion": "5.0\n"
+        },
+        "ports": {
+            "usb2.0slots": "1",
+            "headphoneJack": "Yes",
+            "microphoneJack": "Yes"
+        },
+        "multimedia": {
+            "webcam": "Yes",
+            "videoRecording": "720p\n",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "Built-in Dual Speakers\n",
+            "inbuiltMicrophone": "Yes",
+            "microphoneType": "Integrated Dual Array Digital Microphone",
+            "secondarycam(Rear-facing)": "No"
+        },
+        "peripherals": {
+            "pointingDevice": "Touchpad with Multi-Touch Gestures Enabled\n",
+            "keyboard": "Full-size, Ultra Violet , Shadow Black Keyboard with Numeric Keypad\n",
+            "backlitKeyboard": "Yes",
+            "fingerprintScanner": "No"
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": "Laptop, Battery, AC Adapter, User Guide\n",
+            "warranty": "1 Year\n"
+        },
+        "createdAt": "2023-02-04T13:53:22.908Z"
+    },
+    {
+        "_id": "63de5dc52a98c27336649840",
+        "name": "Infinix INBook X1 Slim XL21 Laptop (Core i3 10th Gen/8 GB/256 GB SSD/Windows 11/128 MB)",
+        "price": "32990",
+        "brand": "Infinix",
+        "category": [
+            "Laptop",
+            "Gaming laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517374572infinix-inbook-x1-slim-xl21-151324-v1-large-1.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517374572infinix-inbook-x1-slim-xl21-151324-v1-large-2.webp",
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675517374573infinix-inbook-x1-slim-xl21-151324-v1-large-3.webp"
+        ],
+        "generalInformation": {
+            "brand": "Infinix",
+            "model": "INBook X1 Slim XL21\n",
+            "thickness": "14.8 Millimeter thickness (Slim)\n",
+            "dimensions(WxDxH)": "323.3 x 211.1 x 14.8  mm\n",
+            "weight": "1.24 Kg weight (Light-weight)\n",
+            "colour(s)": "Starfall Grey\n",
+            "operatingSystem": "Windows 11 Home Basic\n"
+        },
+        "displayDetails": {
+            "displaySize": "14 Inches (35.56 cm)\n",
+            "displayResolution": "1920 x 1080 Pixels\n",
+            "pixelDensity": "157 ppi\n",
+            "displayType": "",
+            "displayFeatures": "FHD\nIPS Display (300nits Peak Brightness\n100% sRGB\nNTSC 72%",
+            "touchScreen": "No"
+        },
+        "performance": {
+            "processor": "Intel Core i3-1005G1 (10th Gen)\n",
+            "clockSpeed": "1.2 Ghz\n",
+            "graphicProcessor": "Intel UHD\n"
+        },
+        "memory": {
+            "capacity": "8 GB\n",
+            "ramType": "LPDDR4X\n",
+            "memorySlots": "1",
+            "memoryLayout": "1*8 Gigabyte"
+        },
+        "storage": {
+            "ssdCapacity": "256 GB"
+        },
+        "battery": {
+            "batteryCell": "Li-Ion\n",
+            "batteryType": "",
+            "powerSupply": "65 W\n"
+        },
+        "networking": {
+            "wirelessLAN": "802.11 a/b/g/n/ac\n",
+            "wifiVersion": "5",
+            "bluetooth": "Yes",
+            "bluetoothVersion": "5.1"
+        },
+        "ports": {
+            "usb2.0slots": "2",
+            "headphoneJack": "",
+            "microphoneJack": "Yes\n"
+        },
+        "multimedia": {
+            "webcam": "Yes\n",
+            "videoRecording": "720p\n",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "Built-in Dual Speakers\n",
+            "inbuiltMicrophone": "Yes",
+            "microphoneType": "Built In Dual Digital Array Microphone",
+            "secondarycam(Rear-facing)": "DTS Audio Processing\n"
+        },
+        "peripherals": {
+            "pointingDevice": "",
+            "keyboard": "",
+            "backlitKeyboard": "Yes",
+            "fingerprintScanner": "No"
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": "Laptop, Power Adaptor, User Guide, Warranty Documents\n",
+            "warranty": "1 Year\n"
+        },
+        "createdAt": "2023-02-03T13:53:22.908Z"
+    },
+    {
+        "_id": "63de75787194b41884d4693d",
+        "name": "Acer Laptop",
+        "price": "1234",
+        "brand": "Dell",
+        "category": [
+            "Gaming laptop",
+            "Laptop"
+        ],
+        "images": [
+            "https://yashyahiya-ecommerce.s3.ap-south-1.amazonaws.com/1675523030179fa284232-1363-42eb-8bd9-fc9d608977d7%20%283%29.jpeg"
+        ],
+        "generalInformation": {
+            "brand": "Dell",
+            "model": "",
+            "thickness": "",
+            "dimensions(WxDxH)": "",
+            "weight": "",
+            "colour(s)": "",
+            "operatingSystem": ""
+        },
+        "displayDetails": {
+            "displaySize": "",
+            "displayResolution": "",
+            "pixelDensity": "",
+            "displayType": "",
+            "displayFeatures": "",
+            "touchScreen": ""
+        },
+        "performance": {
+            "processor": "",
+            "clockSpeed": "",
+            "graphicProcessor": ""
+        },
+        "memory": {
+            "capacity": "",
+            "ramType": "",
+            "memorySlots": "",
+            "memoryLayout": ""
+        },
+        "storage": {
+            "ssdCapacity": ""
+        },
+        "battery": {
+            "batteryCell": "",
+            "batteryType": "",
+            "powerSupply": ""
+        },
+        "networking": {
+            "wirelessLAN": "",
+            "wifiVersion": "",
+            "bluetooth": "",
+            "bluetoothVersion": ""
+        },
+        "ports": {
+            "usb2.0slots": "",
+            "headphoneJack": "",
+            "microphoneJack": ""
+        },
+        "multimedia": {
+            "webcam": "",
+            "videoRecording": "",
+            "Secondarycam(Rear-facing)": "",
+            "speakers": "",
+            "inbuiltMicrophone": "",
+            "microphoneType": ""
+        },
+        "peripherals": {
+            "pointingDevice": "",
+            "keyboard": "",
+            "backlitKeyboard": "",
+            "fingerprintScanner": ""
+        },
+        "others": {
+            "Warranty": "",
+            "salesPackage": ""
+        },
+        "createdAt": "2023-02-04T15:10:48.251Z"
+    }
+]
+
+export default data
